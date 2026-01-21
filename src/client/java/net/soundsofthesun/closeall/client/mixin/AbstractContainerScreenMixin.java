2e9e38ec13da8defd84dabed7da8fc399d522486
sun <sun@bluptr.xyz>
2026-01-21 05:39:10 -0500

    fixed container screen desync

--- src/client/java/net/soundsofthesun/closeall/client/mixin/AbstractContainerScreenMixin.java
+++ src/client/java/net/soundsofthesun/closeall/client/mixin/AbstractContainerScreenMixin.java
@@ -1,22 +1,27 @@
 package net.soundsofthesun.closeall.client.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Objects;
 
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin {
-    @WrapMethod(method = "onMouseClickAction")
-    void mod$onMouseClickAction(Slot slot, ClickType type, Operation<Void> original) {
-        // Minecraft already checks if you're clicking outside the gui for item throwing, so I'm using that.
-        // Throwing items by clicking with them outside the gui still works and does not close the screen.
-        if (Objects.isNull(slot) && type == ClickType.THROW) Minecraft.getInstance().setScreen(null);
-        original.call(slot, type);
+    @WrapOperation(method = "slotClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handleInventoryMouseClick(IIILnet/minecraft/world/inventory/ClickType;Lnet/minecraft/world/entity/player/Player;)V"))
+    void mod$handleMouseClick(MultiPlayerGameMode instance, int containerId, int slotId, int mouseButton, ClickType clickType, Player player, Operation<Void> original, @Local Slot slot) {
+        if (Objects.isNull(slot) && clickType == ClickType.THROW) {
+            if (Minecraft.getInstance().player != null) Minecraft.getInstance().player.closeContainer();
+        } else {
+            original.call(instance, containerId, slotId, mouseButton, clickType, player);
+        }
     }
 }
